77ab4382d2eaae8f5d22183c12246b1447b5d323
Artem Chulkov <acc@arch.pm>
2020-10-26 15:32:33 +0500

    change structure

--- lib/leaser/leaser_test.go
+++ lib/leaser/leaser_test.go
@@ -1,4 +1,4 @@
-package main
+package leaser
 
 import (
 	"testing"
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 
+	"github.com/archekb/gipam/lib/config"
+	"github.com/archekb/gipam/lib/gipam"
+	"github.com/archekb/gipam/lib/leaser"
+
 	"github.com/docker/go-plugins-helpers/ipam"
 )
 
@@ -10,43 +14,43 @@ func init() {
 }
 
 func main() {
-	cnf, err := NewConfig()
+	cnf, err := config.NewConfig()
 	if err != nil {
-		ConfigHelp()
+		config.ConfigHelp()
 		log.Fatalln("Config Error:", err)
 	}
 
 	// new leaser
-	leaser, err := NewLeaser(cnf.Lease.IPv6, cnf.Lease.IPv4, cnf.Lease.IPv6AB, cnf.Lease.IPv4AB)
+	lsr, err := leaser.NewLeaser(cnf.Lease.IPv6, cnf.Lease.IPv4, cnf.Lease.IPv6AB, cnf.Lease.IPv4AB)
 	if err != nil {
 		log.Fatalln("Create Leaser Instance Error:", err)
 	}
 
-	log.Println("Created new AddressSpace. Len main IPv6 address pool -", leaser.V6Pool.SubnetCount(leaser.V6AllocateBlock), "/ Len main IPv4 address pool -", leaser.V4Pool.SubnetCount(leaser.V4AllocateBlock))
+	log.Println("Created new AddressSpace. Len main IPv6 address pool -", lsr.V6Pool.SubnetCount(lsr.V6AllocateBlock), "/ Len main IPv4 address pool -", lsr.V4Pool.SubnetCount(lsr.V4AllocateBlock))
 
 	// Leaser Backup to file
-	leaserBackup, err := NewLeaserBackup(cnf.Lease.File, leaser)
+	lsrBackup, err := leaser.NewLeaserBackup(cnf.Lease.File, lsr)
 	if err != nil {
 		log.Fatalln("Create GIPAM Instance Error:", err)
 	}
 
 	if !cnf.Lease.Wipe {
-		err := leaserBackup.Restore()
+		err := lsrBackup.Restore()
 		if err != nil {
 			log.Fatalln("Restore state Error:", err)
 		}
 	}
 
-	go leaserBackup.Saver()
+	go lsrBackup.Saver()
 
 	// new GIpam
-	gipam, err := NewGIpam(leaser)
+	g, err := gipam.NewGIpam(lsr)
 	if err != nil {
 		log.Fatalln("Create GIPAM Instance Error:", err)
 	}
 
 	log.Println("Start GIPAM driver...")
-	h := ipam.NewHandler(gipam)
+	h := ipam.NewHandler(g)
 	if cnf.Server.UnixSocket {
 		log.Println(h.ServeUnix("gipam", 755))
 	} else {
